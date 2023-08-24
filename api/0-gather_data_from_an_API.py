0fcae7c924f3648b95db9c86787848a5c4fc8603
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-24 10:25:39 -0700

    script that uses a REST API

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+"""
+import requests
+from sys import argv
+
+EMPLOYEE_NAME = ''
+NUMBER_OF_DONE_TASKS = ''
+TOTAL_NUMBER_OF_TASKS = ''
+
+url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
+response = requests.get(url)
+if response.status_code == 200:
+    json_data = response.json()
+    EMPLOYEE_NAME = json_data.get('name')
+
+url = f'https://jsonplaceholder.typicode.com/todos?userId={argv[1]}'
+response = requests.get(url)
+if response.status_code == 200:
+    json_data = response.json()
+    TOTAL_NUMBER_OF_TASKS = len(json_data)
+    complete_tasks = [x for x in json_data if x.get('completed')]
+    NUMBER_OF_DONE_TASKS = len(complete_tasks)
+
+print(f'Employee {EMPLOYEE_NAME} is done with tasks \
+({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
+[print('\t' + x.get('title')) for x in complete_tasks]
