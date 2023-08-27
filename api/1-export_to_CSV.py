ae3a5761d27bf723507f011dae10c9ded80b7791
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-27 12:51:05 -0700

    fit: module csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,23 +5,24 @@ returns information about his/her TODO list progress.
 """
 import requests
 from sys import argv
+from csv import writer, QUOTE_ALL
 
 
 if __name__ == "__main__":
 
     url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
     response = requests.get(url)
-    if response.status_code == 200:
-        json_data = response.json()
-        emp_name = json_data.get('username')
-        emp_id = json_data.get('id')
+    json_data = response.json()
+    emp_name = json_data.get('username')
+    emp_id = json_data.get('id')
 
     url = f'https://jsonplaceholder.typicode.com/todos?userId={argv[1]}'
     response = requests.get(url)
-    if response.status_code == 200:
-        tasks = response.json()
+    tasks = response.json()
 
-    for task in tasks:
-        with open(f"{argv[1]}.csv", 'a') as fil3:
-            fil3.write(f'"{emp_id}", "{emp_name}", "{task.get("completed")}", \
-"{task.get("title")}"' + "\n")
+    data = [[emp_id, emp_name, task.get("completed"), task.get("title")]
+            for task in tasks]
+
+    with open(f"{argv[1]}.csv", 'w') as fil3:
+        writer = writer(fil3, quoting=QUOTE_ALL)
+        writer.writerows(data)
