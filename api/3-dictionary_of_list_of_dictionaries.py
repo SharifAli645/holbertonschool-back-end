45baf0864ec1ef34eea94398c76fe662a66f04f5
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-28 14:20:14 -0700

    Python script to export data in the JSON format

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -3,6 +3,7 @@
 Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
+import json
 import requests
 from sys import argv
 
@@ -12,20 +13,23 @@ if __name__ == "__main__":
     NUMBER_OF_DONE_TASKS = ''
     TOTAL_NUMBER_OF_TASKS = ''
 
-    url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
-    response = requests.get(url)
-    if response.status_code == 200:
-        json_data = response.json()
-        EMPLOYEE_NAME = json_data.get('name')
+    url = f'https://jsonplaceholder.typicode.com/users/'
+    users = requests.get(url).json()
+    user_list = [{"username": user.get('username'), "id": user.get('id')}
+                 for user in users]
 
-    url = f'https://jsonplaceholder.typicode.com/todos?userId={argv[1]}'
-    response = requests.get(url)
-    if response.status_code == 200:
-        json_data = response.json()
-        TOTAL_NUMBER_OF_TASKS = len(json_data)
-        complete_tasks = [x for x in json_data if x.get('completed')]
-        NUMBER_OF_DONE_TASKS = len(complete_tasks)
+    url = f'https://jsonplaceholder.typicode.com/todos/'
+    todos = requests.get(url).json()
 
-    print(f'Employee {EMPLOYEE_NAME} is done with tasks\
-({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
-    [print('\t' + x.get('title')) for x in complete_tasks]
+    dicty = {}
+    for user in user_list:
+        lista = []
+        for todo in todos:
+            if user.get('id') == todo.get('userId'):
+                ele = {"username": user.get('username'),
+                       "task": todo.get('title'),
+                       "completed": todo.get('completed')}
+                lista.append(ele)
+        dicty.update({str(user.get('id')): lista})
+    with open("todo_all_employees.json", 'w') as fil3:
+        fil3.write(json.dumps(dicty))
