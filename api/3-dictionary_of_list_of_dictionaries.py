95908d4a2aca3da7f1d94ca51f00184d2ebac98c
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-28 08:38:39 -0700

    execution permission

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -4,30 +4,28 @@ Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
 import requests
-import json
 from sys import argv
 
 
 if __name__ == "__main__":
+    EMPLOYEE_NAME = ''
+    NUMBER_OF_DONE_TASKS = ''
+    TOTAL_NUMBER_OF_TASKS = ''
 
     url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
     response = requests.get(url)
     if response.status_code == 200:
         json_data = response.json()
-        emp_user = json_data.get('username')
-        emp_id = json_data.get('id')
+        EMPLOYEE_NAME = json_data.get('name')
 
     url = f'https://jsonplaceholder.typicode.com/todos?userId={argv[1]}'
     response = requests.get(url)
     if response.status_code == 200:
         json_data = response.json()
-        tasks = [x for x in json_data]
+        TOTAL_NUMBER_OF_TASKS = len(json_data)
+        complete_tasks = [x for x in json_data if x.get('completed')]
+        NUMBER_OF_DONE_TASKS = len(complete_tasks)
 
-    lista = []
-    for task in tasks:
-        lista.append({"task": task.get('title'),
-                      "completed": task.get('completed'),
-                      "username": emp_user})
-
-    with open(f"{emp_id}.json", 'w') as fil3:
-        fil3.write(json.dumps({emp_id: lista}))
+    print(f'Employee {EMPLOYEE_NAME} is done with tasks\
+({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
+    [print('\t' + x.get('title')) for x in complete_tasks]
