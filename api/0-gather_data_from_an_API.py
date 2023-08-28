5edf7c496a367b05d823b63a2f1f9581c896b474
SharifAli645 <120497885+SharifAli645@users.noreply.github.com>
2023-08-28 10:55:06 -0500

    fit: correct string

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
     done_tasks = [x for x in todo if x.get('completed')]
     num_task = len(done_tasks)
 
-    print(f'Employee {emp_name} is done with tasks ({num_task}/{tasks}):')
+    print(f'Employee {emp_name} is done with tasks({num_task}/{tasks}):')
     for x in done_tasks:
         print(f"\t {x.get('title')}")
